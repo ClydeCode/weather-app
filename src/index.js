c7a8efb2e41930ad3f0980c470a80d17a5d94138
Clyde <noneclyde@gmail.com>
2022-06-25 13:51:01 +0300

    Add event listener to searchfield, function setDetails()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './styles.css';
 import './assets/icons/sunny.png';
-import { getData } from './weather';
+import getData from './weather';
+import { setCity, setSubDetails, setTemp } from './DOM';
+
+const searchField = document.querySelector('#search_field');
+
+async function setDetails(city) {
+  const obj = await getData(city);
+
+  setCity(obj.name);
+  setTemp(obj.temp);
+  setSubDetails(obj.feels, obj.wind, obj.humidity);
+}
+
+searchField.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    setDetails(e.target.value);
+  }
+});
